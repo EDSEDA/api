48b4978746bb3c6784d8ce8e75a402434ddc36d1
Nikita Sulimenko <nikita.sulimenko@smddev.com>
2024-03-19 16:21:34 +0300

    upd kafka client

--- grifon/mqbroker/kafka_client.py
+++ grifon/mqbroker/kafka_client.py
@@ -1,35 +1,34 @@
-import sys
-import six
-
-# need for python3.12
-if sys.version_info >= (3, 12, 0):
-    sys.modules['kafka.vendor.six.moves'] = six.moves
-
 from kafka import KafkaConsumer, KafkaProducer
 import json
 import logging
-from grifon.config import settings
+from grifon.config import settings      # noqa
 
 
 class KafkaClient:
     def __init__(self, topic, bootstrap_servers=f'127.0.0.1:{9092}', producer_config={}, consumer_config={}):
         self.topic = topic
-        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
-                                      value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                                      api_version=(0, 10),
-                                      **producer_config)
-        self.consumer = KafkaConsumer(self.topic,
-                                      bootstrap_servers=bootstrap_servers,
-                                      auto_offset_reset='earliest',
-                                      value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-                                      api_version=(0, 10),
-                                      **consumer_config)
+        self.group_id = 'test_group_id'
+
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+            acks='all',  # Подтверждение от всех реплик
+            **producer_config
+        )
+        self.consumer = KafkaConsumer(
+            self.topic,
+            bootstrap_servers=bootstrap_servers,
+            auto_offset_reset='earliest',
+            value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+            group_id=self.group_id,  # Указание идентификатора группы потребителей
+            **consumer_config
+        )
 
     def send_message(self, message):
         try:
-            self.producer.send(self.topic, message)
-            self.producer.flush()
-            logging.info(f"Сообщение отправлено в топик {self.topic}: {message}")
+            future = self.producer.send(self.topic, message)
+            result = future.get(timeout=60)  # Ожидание завершения отправки
+            logging.info(f"Сообщение отправлено в топик {self.topic}: {result}")
         except Exception as e:
             logging.error(f"Ошибка при отправке сообщения: {e}")
 
@@ -39,6 +38,7 @@ class KafkaClient:
             for message in self.consumer:
                 logging.info(f"Получено сообщение: {message.value}")
                 process_func(message.value)
+                self.consumer.commit()  # Ручной коммит смещения
         except Exception as e:
             logging.error(f"Ошибка при получении сообщений: {e}")
         finally:
@@ -54,7 +54,7 @@ def process_message(message):
     logging.info(f"Обработка сообщения: {message}")
 
 
-if __name__ == "__main__":
+def test_kafka_process_message():
     topic = 'test_topic'
     kafka_client = KafkaClient(topic=topic)
 
@@ -63,3 +63,7 @@ if __name__ == "__main__":
     # # Прослушивание топика и обработка сообщений
     # # Внимание: следующая строка блокирует выполнение, расскомментируйте для использования
     kafka_client.listen_messages(process_message)
+
+
+if __name__ == "__main__":
+    test_kafka_process_message()
