e63bdb70a743c4f926002eddc1a470b2e4013039
Nikita Sulimenko <nikita.sulimenko@smddev.com>
2024-03-28 16:41:15 +0300

    add full test

--- grifon/mqbroker/tests/test_producer.py
+++ grifon/mqbroker/tests/test_producer.py
@@ -1,21 +1,30 @@
 import asyncio
 
+from pydantic import BaseModel
 
 from grifon.mqbroker.kafka_client import KafkaClient
 
 kafka_client = KafkaClient("localhost:9092")
 
 
-async def send_test_message(topic: str, message: str):
+async def send_test_message(topic: str, message: str | BaseModel):
     kafka_client.send_message(topic, message)
     kafka_client.flush()
     print(f"Sent message: {message} to topic {topic}")
 
 
+class TestMessage(BaseModel):
+    id: int
+    name: str
+
+
+m = TestMessage(id=12, name='Nik')
+
+
 async def main():
 
     # Отправка тестового сообщения в топики
-    await send_test_message("my_topic1", "Hello, Kafka to topic1!")
+    await send_test_message("my_topic1", m)
     await send_test_message("my_topic2", "Hello, Kafka to topic2!")
 
 
