abade332044b9dbd374b5b68a8127320a0860ca4
Aleksandr Popov <alv.popov@vk.team>
2024-04-01 22:25:36 +0300

    Delete embeddings and fix examples for recommendation create event

--- grifon/recommendation/schema.py
+++ grifon/recommendation/schema.py
@@ -1,17 +1,14 @@
 from pydantic import BaseModel, Field
 
-from typing import List
-
 from .enums import SexEnum
 from .enums import RaceEnum
 from .enums import EmotionEnum
 
 
 class CreateUserRecommendationMessage(BaseModel):
-    cash_register_id: int = Field(description='ID кассы', example='123')
-    embedding: List[float] = Field(description='Эмбеддинг юзера', example='[0.0, 0.0, 0.0]')
-    user_id: int = Field(description='ID юзера', example='123')
-    age: int = Field(description='Возраст', example='12')
-    sex: SexEnum = Field(description='Пол', example='M')
-    race: RaceEnum = Field(description='Раса', example='European')
-    emotion: EmotionEnum = Field(description='Текущая эмоция', example='Happy')
+    cash_register_id: int = Field(description='ID кассы', example=123)
+    user_id: int = Field(description='ID юзера', example=123)
+    age: int = Field(description='Возраст', example=123)
+    sex: SexEnum = Field(description='Пол', example='male')
+    race: RaceEnum = Field(description='Раса', example='white')
+    emotion: EmotionEnum = Field(description='Текущая эмоция', example='neutral')
